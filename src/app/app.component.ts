87f925e3e200d1bea34dfc0a2c064a1aff3e6c8a
Scott <70402467+SWilli93@users.noreply.github.com>
2024-05-01 20:20:06 -0400

    completion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Book } from './book.model';
+import { BookService } from './book.service';
+import { NgFor } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NgFor],
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'books';
+
+export class AppComponent implements OnInit {
+  title= "Books About Banking";
+  books: Book[] = [];
+
+  constructor(private bookservice: BookService) {}
+
+  ngOnInit() {
+      const booksObservable = this.bookservice.getBooks();
+      booksObservable.subscribe((booksData: Book[]) => {
+          this.books = booksData;
+      });
+  }
 }
